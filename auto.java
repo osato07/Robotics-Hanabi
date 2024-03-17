b8907fc46f7ab5afcf6ed976c8602ea5a32283b4
osatoosi <126332608+osato07@users.noreply.github.com>
2024-03-17 20:29:52 +0900

    Create auto.java

---
+++ auto.java
@@ -0,0 +1,5 @@
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
