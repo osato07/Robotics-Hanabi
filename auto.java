6a4cb157cc89d47de43b5de994eb507569ff50c0
osatoosi <126332608+osato07@users.noreply.github.com>
2024-03-17 20:35:52 +0900

    Update auto.java

--- auto.java
+++ auto.java
@@ -1,5 +1,78 @@
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+package frc.robot;
 
+import edu.wpi.first.wpilibj.Timer;
+// import java.util.Optional;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+// import com.ctre.phoenix.motorcontrol.TalonSRXSimCollection;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+// import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.TimedRobot;
+// import edu.wpi.first.wpilibj.DriverStation;
+// import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Servo;
+
+public class Robot extends TimedRobot {
+    private static final String kDefaultAuto = "Default";
+    private static final String kCustomAuto = "My Auto";
+    private String m_autoSelected;
+    private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+    @Override
+    public void robotInit() {
+        m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+        m_chooser.addOption("My Auto", kCustomAuto);
+        SmartDashboard.putData("Auto choices", m_chooser);
+    }
+
+    /** This function is run once each time the robot enters autonomous mode. */
+    @Override
+    public void autonomousInit() {
+        m_autoSelected = m_chooser.getSelected();
+        System.out.println("Auto selected: " + m_autoSelected);
+        m_timer.restart();
+    }
+
+    /** This function is called periodically during autonomous. */
+    @Override
+    public void autonomousPeriodic() {
+        switch (m_autoSelected) {
+          case kCustomAuto:
+            if (m_timer.get() < 2.0) {
+                System.out.println("autoMa");
+            } else {
+                System.out.println("autoMi");
+            }
+            new WaitCommand(5.0);
+            
+            break;
+          case kDefaultAuto:
+          default:
+            if (m_timer.get() < 2.0) {
+                System.out.println("autoMa");
+            } else {
+                System.out.println("autoMi");
+            }
+            new WaitCommand(5.0);
+            
+            break;
+        }
+    }
+
+    @Override
+    public void teleopPeriodic() {}
+
+    /** This function is called once each time the robot enters test mode. */
+    @Override
+    public void testInit() {}
+
+    /** This function is called periodically during test mode. */
+    @Override
+    public void testPeriodic() {
+        
+    }
+}
